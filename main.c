25976f50674d2aca0f7698d508246458651f3482
Matheus Torres <110057634+Torress1@users.noreply.github.com>
2022-08-29 21:41:50 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+int ler (){
+    int i;
+    printf ("Digite a idade (0 a 110):");
+    scanf ("%d", &i);
+    while (i< 0 || i > 110){
+        printf ("Digite a idade (0 a 110):");
+        scanf ("%d", &i);
+    }
+    return i;
+        }
+int main(){
+  int id1, id2;
+  id1= ler();
+  id2= ler();
+  
+  printf("dif""%d", id1 - id2);
+  return 0;
+}
+
